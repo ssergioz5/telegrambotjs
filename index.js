730e91eff646993d3645c8bb544188ad3e04091d
root <root@telegrambot.europe-west4-a.c.telegram-nodejs.internal>
2020-03-27 21:18:10 +0000

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = '1119562512:AAFi6Tl1umIcYcRty-ifaVN8jww2fmjKUlw';
 var bot = new TelegramBot(token, {polling:true});
+var test = 'hola'
 bot.onText(/\/echo (.+)/,function(msg,match){
     var chatId = msg.chat.id;
     var echo = match[1];
